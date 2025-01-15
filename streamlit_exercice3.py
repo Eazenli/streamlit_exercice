8b7428f80cf6f31c3b685702ebfb7841f8997a4c
Eazenli <yizhenli233@gmail.com>
2025-01-15 16:02:42 +0100

    Update streamlit_exercice3.py
    Create a sidebar to navigate the menu

--- streamlit_exercice3.py
+++ streamlit_exercice3.py
@@ -1,6 +1,5 @@
 import streamlit as st
 from streamlit_authenticator import Authenticate
-from streamlit_option_menu import option_menu
 
 users = {'usernames': {'utilisateur': {'name': 'utilisateur',
                                        'password': 'utilisateurMDP',
@@ -17,32 +16,30 @@ authenticator = Authenticate(
 authenticator.login()
 
 if st.session_state.get("authentication_status"):
-    authenticator.logout("Déconnexion")
-    st.title(f"Bienvenue {users['usernames']['utilisateur']['name']}")
 
-    selection = option_menu(
-        menu_title=None,
-        options=["Accueil", "Photos"],
-        orientation="horizontal"
-    )
+    with st.sidebar:
+        if st.sidebar.button("Déconnexion"):
+            authenticator.logout("Déconnexion")
+        st.write(f"Bienvenue {users['usernames']['utilisateur']['name']}")
+        selection = st.sidebar.radio("Menu", ["🏠Accueil", "🐱GIF"])
 
-    if selection == "Accueil":
-        st.write("Bienvenue sur ma page !")
+    if selection == "🏠Accueil":
+        st.header("Bienvenue sur ma page ! 👏")
         st.image(r"https://media3.giphy.com/media/v1.Y2lkPTc5MGI3NjExODZlcTN2OWY4NjBhaWduN294MDI5Z2FmZnFmdmViNjgzcjdjZG5qdCZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/GRPy8MKag9U1U88hzY/giphy.gif")
 
-    elif selection == "Photos":
-        st.write("Bienvenue sur mon album photo")
+    elif selection == "🐱GIF":
+        st.header("Les GIFs de chat 🐱")
         col1, col2, col3 = st.columns(3)
         with col1:
-            st.header("A cat")
+            st.write("Laser cat")
             st.image(r"https://media2.giphy.com/media/v1.Y2lkPTc5MGI3NjExZDV1ZW5lc2IyZmY3MDh1c21ieGF3am5lbzN3dGpnZW1xNzNvcjQxdCZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/3o85xoi6nNqJQJ95Qc/giphy.gif")
 
         with col2:
-            st.header("Another cat")
+            st.write("Hard-working cat")
             st.image(r"https://media0.giphy.com/media/v1.Y2lkPTc5MGI3NjExOWE2aGt6NHA1aTJmY3Z5ZmI3azFrcW9yOWtrdHV1c3hzbHF6NTQ2MiZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/lJNoBCvQYp7nq/giphy.gif")
 
         with col3:
-            st.header("Another cat")
+            st.write("Nail-filing cat")
             st.image(r"https://media1.giphy.com/media/v1.Y2lkPTc5MGI3NjExdmh1emM1YjZhMzE5c3V0eXoxaDJ3ZHVsbmcwZ2JmdWk0aHZmMWQxZCZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/mlvseq9yvZhba/giphy.gif")
 
 elif st.session_state.get("authentication_status") is False:
